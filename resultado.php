ba31a0cb69650ba198f2b8f5408b514e1109a9c6
Jose Antonio Dominguez Camacho <joseantonio.dominguez@iesdonana.org>
2024-10-10 15:12:13 +0200

    CalculadoraV2

--- resultado.php
+++ resultado.php
@@ -1,37 +1,55 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado</title>
 </head>
+
 <body>
-    <p>
-        <?php
-        if (isset($_GET['op1']) && !empty($_GET['op1']) && is_numeric($_GET['op1'])) {
-            $op1 = trim($_GET['op1']);
+<form action="resultado.php" method="get">
+        <label>Primer operando:
+            <input type="text" name="op1">
+        </label>
+        <br>
+        <label>Segundo operando:
+            <input type="text" name="op2">
+        </label>
+        <br>
+        <label>Operación:
+            <input type="text" name="op">
+        </label>
+        <br>
+        <button type="submit">Calcular</button>
+    </form>
+
+    <?php
+    require 'auxiliar.php';
+
+    $errores = [];
+
+    if (!isset($_GET['op1'], $_GET['op2'], $_GET['op'])) {
+    
+    ?>
+        <?= "" ?>
+    
+    <?php
+
+    } else {
+        $op1 = comprobar_primer_operando($errores);
+        $op2 = comprobar_segundo_operando($errores);
+        $op = comprobar_operacion($op2, $errores);
+    
+        if (hay_errores($errores)) {
+            mostrar_mensajes_error($errores);
         } else {
-            echo "<h3>El primer operando no es correcto.</h3>";
+            $res = calcular_operacion($op1, $op2, $op);
+            mostrar_resultado($op1, $op2, $op, $res);
         }
-
-        if (isset($_GET['op2']) && !empty($_GET['op2']) && is_numeric($_GET['op2'])) {
-            $op2 = trim($_GET['op2']);
-        } else {
-            echo "<h3>El segundo operando no es correcto.</h3>";
-        }
-
-        if (isset($_GET['op']) && !empty($_GET['op'])) {
-
-
-        if (isset($op1, $op2)) {
-            $res = $op1 + $op2;  // int|float
-
-            $mensaje = "La suma de $op1 y $op2 es $res.";
-            ?>
-            <?= $mensaje ?>
-        <?php
-        }
-        ?>
-    </p>
+    }
+    ?>
 </body>
+
 </html>
